5436533a5c0cb54e31f469525202fa2f71278bd8
Rodolphe Desruelles <rodriced@free.fr>
2022-09-27 01:14:46 +0200

    Initial Commit

---
+++ Swimmee/SwimmeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwimmeeApp.swift
+//  Swimmee
+//
+//  Created by Rodolphe Desruelles on 27/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwimmeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
